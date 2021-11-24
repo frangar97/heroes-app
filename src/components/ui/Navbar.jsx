a689ebe4d579892cb95f9fe9c4dcbc903e208781
Francisco A. Garcia <frangarcia38@hotmail.com>
2021-11-24 14:34:31 -0600

    Initial Commit

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const handleLogout = () => {
+        //TODO: implementar logica logout
+        console.log("logout")
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+
+            <Link
+                className="navbar-brand"
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink
+                        className={({ isActive }) => isActive ? "nav-item nav-link active" : "nav-item nav-link"}
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink
+                        className={({ isActive }) => isActive ? "nav-item nav-link active" : "nav-item nav-link"}
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+                    <span className="nav-item nav-link text-info">Francisco</span>
+
+                    <button className="nav-item nav-link btn" onClick={handleLogout}>
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
