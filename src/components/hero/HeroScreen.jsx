d48085ca9ae20e7cd68e3dad7e1aefc91888d279
Francisco A. Garcia <frangarcia38@hotmail.com>
2021-11-25 08:04:56 -0600

    Get hero by id

--- src/components/hero/HeroScreen.jsx
+++ src/components/hero/HeroScreen.jsx
@@ -1,7 +1,17 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "selectors/getHeroById";
+
 export const HeroScreen = () => {
+    const { heroeId } = useParams();
+    const hero = getHeroById(heroeId);
+
+    if (!hero) {
+        return <Navigate to="/" />
+    }
+
     return (
         <div>
-            <h1>Hero Screen</h1>
+            <h1>{hero.superhero}</h1>
         </div>
     )
 }
