fc307f4e52443b1240138c998b19c710bc7b8e5d
Francisco <frangarcia38@hotmail.com>
2021-11-28 07:59:40 -0600

    Implementando context api para determinar usuario logeado

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,7 +1,24 @@
-import { AppRouter } from "routers/AppRouter"
+import { useEffect, useReducer } from "react";
+import { AppRouter } from "routers/AppRouter";
+import { AuthContext } from 'auth/authContext';
+import { authReducer } from "auth/authReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem("user")) || { logged: false };
+}
 
 export const HeroesApp = () => {
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
+    useEffect(() => {
+        if (!user) return;
+
+        localStorage.setItem("user", JSON.stringify(user))
+    }, [user])
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{ user, dispatch }}>
+            <AppRouter />
+        </AuthContext.Provider>
     )
 }
--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,9 +1,14 @@
+import { AuthContext } from "auth/authContext";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { types } from "types/types";
 
 export const LoginScreen = () => {
+    const { dispatch } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleLogin = () => {
+        dispatch({ type: types.login, payload: { nombre: "Francisco" } });
         navigate("/marvel", { replace: true });
     }
 
